1fd31a6da1e9b64c8ebfd5376cd9b5624f88b45c
Jörn Schmidt <schmidt@devid.net>
2015-07-10 09:20:55 +0200

    update pods

--- Pods/Target Support Files/Pods-SecucardConnectClient-AFNetworking/Pods-SecucardConnectClient-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SecucardConnectClient_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_SecucardConnectClient_AFNetworking
-@end
